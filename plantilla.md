6c11d4dbab444924910d1804e9136cc9934f9cb9
Alejandro Villar <avillar@ticnor.es>
2024-03-19 12:30:42 +0100

    Versión inicial con noticia de prueba

---
+++ plantilla.md
@@ -0,0 +1,8 @@
+title: Título de la noticia
+date: 2024-03-19
+
+Esto es un ejemplo de noticia que usa [Markdown](https://daringfireball.net/projects/markdown/).
+
+Se pueden introducir imágenes:
+
+![Descripción de la imagen](https://pbs.twimg.com/media/GHgZ8psWcAAtOha?format=jpg&name=large)
---
+++ scripts/compile.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import json
+from os.path import relpath
+from pathlib import Path
+import re
+from urllib.parse import quote
+
+from markdown import Markdown
+
+BASE_URL = 'https://ticnor.github.io/gopastor-noticias/'
+
+md = Markdown(extensions=['meta'])
+
+
+def image_url(url: str, md_path: Path):
+    if re.match(f'^https?://', url):
+        return url
+    return f"{BASE_URL}{quote(relpath(md_path.parent.joinpath(url)))}"
+
+
+posts = []
+for fn in Path('posts').rglob('*.md'):
+    with open(fn) as f:
+        html = md.convert(f.read())
+    metadata = md.Meta
+    posts.append({
+        'filename': str(fn),
+        'title': metadata['title'][0],
+        'images': [image_url(i, fn) for i in metadata.get('image', [])],
+        'date': metadata['date'][0],
+        'content': html,
+    })
+
+posts.sort(reverse=True, key=lambda x: x['filename'])
+
+output = Path('dist/noticias.json')
+output.parent.mkdir(parents=True, exist_ok=True)
+with open(output, 'w') as f:
+    json.dump(posts, f)
